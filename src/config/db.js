432f21896027503727dd9461229acb22aa48c712
Anton <anton.sipin@gmail.com>
2020-12-28 17:16:13 +0300

    Added DB connect

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const dbConnectionURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wrnxb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 function dbConnect() {
-  mongoose.createConnection(dbConnectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  mongoose.connect(dbConnectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) return console.log(err)
     return console.log(`Success connected to ${process.env.DB_NAME} database`)
   })
