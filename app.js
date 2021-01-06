53fa6a8c7aaf05436b2024e925524c8519801b96
Anton <anton.sipin@gmail.com>
2021-01-06 13:40:28 +0300

    Added NODE_ENV

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV,
     maxAge: 1000 * 60 * 60 * 24
   },
 })); 
