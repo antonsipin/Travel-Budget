666db3efdc00d49cb1d44d0db661b8efb7c3efd9
Anton <anton.sipin@gmail.com>
2023-10-29 02:27:42 +0300

    Fixed send emails

--- src/controllers/mail-controller.js
+++ src/controllers/mail-controller.js
@@ -1,31 +1,50 @@
-const dotenv = require('dotenv')
-dotenv.config()
-const nodemailer = require("nodemailer")
+require('dotenv').config()
+const nodemailer = require('nodemailer')
 
 const renderSendMailForm = (req, res) => {
   res.render('testMail')
 }
 
 const sendMail = async (req, res) => {
-  let transporter = nodemailer.createTransport({
-    host: "smtp.mail.ru",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.mailuser,
-      pass: process.env.mailpassword
-    },
-  });
+  try {
+      const { id } = req.query
+      const URL = `http://${process.env.HOST}:${process.env.PORT}/newtrip/${id}`
 
-  await transporter.sendMail({
-    from: `"From Travel Budget" <${process.env.mailuser}>`,
-    to: "testabc.testabc@mail.ru",
-    subject: "Here is your report ✔",
-    text: "Here is your report",
-    html: `Here is your report`
-  });
+      let transporter = nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT || 465,
+        secure: true,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASSWORD
+        },
+      })
 
-res.redirect('/')
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(error)
+        } else {
+          console.log('Server is ready to take our messages')
+        }
+      })
+
+  
+        const result = await transporter.sendMail({
+          from: `"From Travel Budget" <${process.env.MAIL_USER}>`,
+          to: `${process.env.MAIL_USER}`,
+          subject: 'Here is your report ✔',
+          text: `Here is your report`,
+          html: `<p>Here is your report: ${URL}</p>`
+        })
+        if (result.accepted.length) {
+          console.log('The report has been sent')
+        } else {
+          console.log('The report not sent')
+        }
+        res.redirect('/')
+    } catch (e) {
+      console.log(e)
+    }
 }
 
 module.exports = {
